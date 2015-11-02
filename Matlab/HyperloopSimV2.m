a947f4953ac552bc00aa546ec6cd2eda9acfaa2c
falafel2222 <abg.goldstein@gmail.com>
2015-11-01 17:22:43 -0800

    no longer falls through floor.

--- Matlab/HyperloopSimV2.m
+++ Matlab/HyperloopSimV2.m
@@ -3,33 +3,34 @@
 function [] = HyperloopSimV2()
     disp('Simulation Started')
     %set initial variables
-        %pod dimensions
-        podHeight = 1; %m
-        podWidth = 1; %m
-        podLength = 1.3; %m
-        skateLength = podLength; %m
-    
-        mass = 3000; %kg
-        I = [1.0/12*mass*(podHeight^2 + podWidth^2) 0 0; ...
-             0 1.0/12*mass*(podHeight^2 + podLength^2) 0; ...
-             0 0 1.0/12*mass*(podLength^2 + podWidth^2)];
-        CoM = [0,0,0];
-        
-        timestep = .001; %sec
-        maxTime = 10; %sec
-        numSteps = maxTime / timestep;
-        
-        % pod state variables
-        transPos = zeros(3, numSteps);
-        transVel = zeros(3, numSteps);
-        transAcc = zeros(3, numSteps);
-        
-        rotPos = zeros(3, numSteps);
-        rotVel = zeros(3, numSteps);
-        rotAcc = zeros(3, numSteps);
-        
-        q=zeros(4, numSteps);
-        q(:,1)=[0.00199999; 0.00199999; 0.00199999; 0.999994];
+    %pod dimensions
+    podHeight = 1; %m
+    podWidth = 1; %m
+    podLength = 4; %m
+    skateLength = podLength; %m
+
+    mass = 1500; %kg
+    I = [1.0/12*mass*(podHeight^2 + podWidth^2) 0 0; ...
+         0 1.0/12*mass*(podHeight^2 + podLength^2) 0; ...
+         0 0 1.0/12*mass*(podLength^2 + podWidth^2)];
+    CoM = [0,0,0];
+
+    timestep = .0001; %sec
+    maxTime = 5; %sec
+    numSteps = maxTime / timestep;
+
+    % pod state variables
+    transPos = zeros(3, numSteps);
+    transVel = zeros(3, numSteps);
+    transAcc = zeros(3, numSteps);
+    transPos(3,1) = -.0832;
+
+    rotPos = zeros(3, numSteps);
+    rotVel = zeros(3, numSteps);
+    rotAcc = zeros(3, numSteps);
+
+    q=zeros(4, numSteps);
+    q(:,1)=[0;0;.0; 1];
         
 
 
@@ -46,20 +47,25 @@ function [] = HyperloopSimV2()
     
                     
     %thrust generating points
-    airSkateRight=zeros(3,10);
-    airSkateLeft=zeros(3,10);
-    for i=1:length(airSkateRight)
-       airSkateRight(:,i+1) = [podLength/2-i*skateLength/10,-podWidth/2,-podHeight/2];
-       airSkateLeft(:, i+1) = [podLength/2-i*skateLength/10, podWidth/2,-podHeight/2];
-    end
-    skatePoints=[airSkateLeft airSkateRight];                
+    numSegments = 2;
     
+    airSkateRight=zeros(3,numSegments);
+    airSkateLeft=zeros(3,numSegments);
+    for i=1:numSegments
+       airSkateRight(:,i) = [podLength/2-(i-1)*skateLength/(numSegments-1),-podWidth/2,-podHeight/2];
+       airSkateLeft(:, i) = [podLength/2-(i-1)*skateLength/(numSegments-1), podWidth/2,-podHeight/2];
+    end
+    
+    skatePoints=[airSkateLeft airSkateRight];                
+        
     disp('Simulation Initialized')
     %%%BEGIN LOOPING THROUGH TIMESTEPS%%%
     for n = 2:numSteps
-        if mod(n,100)==0
+        if mod(n,10) == 0
+            disp('--------------------------')
             disp(n/1000)
-            disp(transPos(:,n))
+            disp(transPos(:,n-1))
+%             disp(q(:,n-1))
         end
         
         
@@ -83,28 +89,33 @@ function [] = HyperloopSimV2()
             skateForces=zeros(3,length(skatePoints));
           
             for i=1:length(skatePoints)
-                point=skatePoints(:,i);
+                point= rotMatrix*skatePoints(:,i) + transPos(:,n-1);
                 [~, vertDist]=DistanceFinder(point);
                 
-                %%%%THIS LINE ISN'T RIGHT, STILL SAME MAGNITUDE%%%%
-                vertDist=rotMatrix*vertDist;
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                %%% TODO: change the magnitude of vertDistance if the pod
+                %%% is rotated
                 
                 %pass 0 internal pressure for now
-                pointForce=SkateForce(vertDist(3),0);
-                skateForces(:,i)=pointForce/length(airSkateRight);
+                pointForce=SkateForce(vertDist,0);
+                
+%                 if i == 1 && mod(n,1) == 0
+%                     vertDist
+%                 end
+                
+                skateForces(3,i)=pointForce/(2*length(airSkateRight));
             end
             localForces=[localForces skateForces];
             localPoints=[localPoints skatePoints];
              
+            
             %forces in global
             
             %%%GRAVITY IS EXAMPLE, ACTUALLY TRIVIAL%%%
-            gravityForce=[0 0 -9.8];
+            gravityForce=[0 0 -9.8]* mass;
             gravityPoint=[0 0 0];
             localGravityForce=rotMatrix\gravityForce';
             localGravityPoint=rotMatrix\gravityPoint';
-           
+
             localForces=[localForces localGravityForce];
             localPoints=[localPoints localGravityPoint];
            
@@ -118,33 +129,37 @@ function [] = HyperloopSimV2()
         
         %get theta accel by tensor\torque
         rotAcc(:,n) = I \ transpose(netTorque);
-            %update omega, calculate omega matrix, update quaternions
-            rotVel(:,n) = rotVel(:,n-1) + timestep * rotAcc(:,n);
-            
-            OmegaMatrix=[0 rotVel(3,n) -rotVel(2,n) rotVel(1,n);...
-                        -rotVel(3,n) 0 rotVel(1,n) rotVel(2,n);...
-                         rotVel(2,n) -rotVel(1,n) 0 rotVel(3,n);...
-                        -rotVel(1,n) -rotVel(2,n) -rotVel(3,n) 0;];
-                    
-            qn=q(:,n-1)+0.5*OmegaMatrix*q(:,n-1);
-            normQuat=sqrt(sum(qn.^2));
-            qn=qn./normQuat;
-            normQuat=sqrt(sum(qn.^2));
-            
-            %calculate roll, pitch, and yaw
-            rotPos(:,n-1) = [atan2(2*(q0*q1+q2*q3),1-2*(q1^2 + q2^2)); ...
-                             asin(2*(q0*q2 - q3*q1));...
-                             atan2(2*(q0*q3+q1*q2),1-2*(q2^2 + q3^2))];
-            
+        
+        %update omega, calculate omega matrix, update quaternions
+        rotVel(:,n) = rotVel(:,n-1) + timestep * rotAcc(:,n);
+
+        OmegaMatrix=[0 rotVel(3,n) -rotVel(2,n) rotVel(1,n);...
+                    -rotVel(3,n) 0 rotVel(1,n) rotVel(2,n);...
+                     rotVel(2,n) -rotVel(1,n) 0 rotVel(3,n);...
+                    -rotVel(1,n) -rotVel(2,n) -rotVel(3,n) 0;];
+
+        qn=q(:,n-1)+0.5*OmegaMatrix*q(:,n-1);
+        normQuat=sqrt(sum(qn.^2));
+        qn=qn./normQuat;
+        
+        
+        q(:,n) = qn;
+
+        %calculate roll, pitch, and yaw
+        rotPos(:,n-1) = [atan2(2*(q0*q1+q2*q3),1-2*(q1^2 + q2^2)); ...
+                         asin(2*(q0*q2 - q3*q1));...
+                         atan2(2*(q0*q3+q1*q2),1-2*(q2^2 + q3^2))];
+
         %get forces in global
         globalForces=rotMatrix*localForces;
+        
         netForce=[0 0 0];
         for force=globalForces
-           netForce = netForce+force';
+            netForce = netForce+force';
         end
-        if mod(n,1000)==0
-            globalForces
-        end
+%         if mod(n,1)==0
+%             netForce
+%         end
         
         %get accel, velocity, position
         transAcc(:,n)=netForce/mass;
@@ -153,9 +168,9 @@ function [] = HyperloopSimV2()
         
         %check collisions
         endSimul=false;
-        for point=collisionPoints';
+        for point = collisionPoints';
+            point = rotMatrix*point + transPos(:,n);
             [distance, ~]=DistanceFinder(point);
-
             if distance<=0
                 disp('Collision Occurred at timestep');
                 disp(n);
@@ -170,5 +185,5 @@ function [] = HyperloopSimV2()
                 
     end
     
-    
+    plot(transPos(3,:));
        
